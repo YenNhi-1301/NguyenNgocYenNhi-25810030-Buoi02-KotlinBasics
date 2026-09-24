dbe8cc279a276197e5ca9fb80238664474282450
Yen Nhi <25810030@student.hcmute.edu.vn>
2026-09-24 15:34:59 +0700

    Bai 02 - Gioi thieu ban than

---
+++ Buoi02_Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,10 @@
+// Nguyen Ngoc Yen Nhi - 25810030
+fun main() {
+    val hoTen: String = "Nguyen Ngoc Yen Nhi"
+    val namSinh: Int = 2002
+    val namHienTai: Int = 2026
+
+    val tuoi: Int = namHienTai - namSinh
+
+    println("Xin chao, toi la $hoTen, nam nay toi $tuoi tuoi.")
+}
