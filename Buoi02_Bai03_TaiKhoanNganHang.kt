4bea80aec8e67cc25dc90165e34731a825a699b8
Yen Nhi <25810030@student.hcmute.edu.vn>
2026-09-24 15:35:38 +0700

    Bai 03 - Tai khoan ngan hang

---
+++ Buoi02_Bai03_TaiKhoanNganHang.kt
@@ -0,0 +1,14 @@
+// Nguyen Ngoc Yen Nhi - 25810030
+fun main() {
+    val soDuBanDau: Int = 3000000
+    var soDuHienTai: Int = soDuBanDau
+
+    // val vi so du ban dau khong thay doi
+    // var vi so du hien tai thay doi sau moi giao dich
+
+    soDuHienTai = soDuHienTai + 2000000
+    println("Sau khi gui 2 trieu: $soDuHienTai VND")
+
+    soDuHienTai = soDuHienTai - 1500000
+    println("Sau khi rut 1 trieu 5: $soDuHienTai VND")
+}
